2336ef9edba761330c55c8d66f121739479b62f2
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-21 21:07:06 +0100

    feat: add square peg class (design pattern: Adapter)

---
+++ src/main/java/structural/adapter/square/SquarePeg.java
@@ -0,0 +1,19 @@
+package structural.adapter.square;
+
+public class SquarePeg {
+    private final double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
