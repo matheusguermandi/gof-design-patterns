300db9fd2a618b979d39692af9c1574d1c99de0b
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-21 21:07:29 +0100

    feat: add square peg adapter (design pattern: Adapter)

---
+++ src/main/java/structural/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,20 @@
+package structural.adapter.adapters;
+
+import structural.adapter.round.RoundPeg;
+import structural.adapter.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
