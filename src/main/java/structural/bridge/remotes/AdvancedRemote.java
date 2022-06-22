a388eb332fb64f933c2b7f71b04de298470dfae4
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-22 21:19:20 +0100

    feat: add remote package (design pattern: Bridge)

---
+++ src/main/java/structural/bridge/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package structural.bridge.remotes;
+
+import structural.bridge.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ src/main/java/structural/bridge/remotes/Remote.java
@@ -0,0 +1,9 @@
+package structural.bridge.remotes;
+
+public interface Remote {
+    void power();
+    void volumeDown();
+    void volumeUp();
+    void channelDown();
+    void channelUp();
+}
