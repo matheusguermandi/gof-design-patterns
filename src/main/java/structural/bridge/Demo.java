d06c9bdf36bbb26c9b8d0ade54a4dc1ba3059736
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-22 21:19:25 +0100

    feat: add Demo (design pattern: Bridge)

---
+++ src/main/java/structural/bridge/Demo.java
@@ -0,0 +1,27 @@
+package structural.bridge;
+
+import structural.bridge.devices.Device;
+import structural.bridge.devices.Radio;
+import structural.bridge.devices.Tv;
+import structural.bridge.remotes.AdvancedRemote;
+import structural.bridge.remotes.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
