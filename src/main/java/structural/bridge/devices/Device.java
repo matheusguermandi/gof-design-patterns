d29715ad45a2ca772d5f26cc8085c0caf4ccdbc5
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-22 21:19:10 +0100

    feat: add device package (design pattern: Bridge)

---
+++ src/main/java/structural/bridge/devices/Device.java
@@ -0,0 +1,12 @@
+package structural.bridge.devices;
+
+public interface Device {
+    boolean isEnabled();
+    void enable();
+    void disable();
+    int getVolume();
+    void setVolume(int percent);
+    int getChannel();
+    void setChannel(int channel);
+    void printStatus();
+}
