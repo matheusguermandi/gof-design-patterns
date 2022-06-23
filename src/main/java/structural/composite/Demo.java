23cbe33155e6111f809f194d39c614474c17e65a
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-23 17:43:15 +0100

    feat: add Demo (design pattern: Composite)

---
+++ src/main/java/structural/composite/Demo.java
@@ -0,0 +1,32 @@
+package structural.composite;
+
+import structural.composite.editor.ImageEditor;
+import structural.composite.shapes.Circle;
+import structural.composite.shapes.CompoundShape;
+import structural.composite.shapes.Dot;
+import structural.composite.shapes.Rectangle;
+
+import java.awt.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
