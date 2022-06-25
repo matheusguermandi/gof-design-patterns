f009e8b031b87a05143d75a5d8d8036306b096c7
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-25 12:37:35 +0100

    feat: add fake libraries package (design pattern: Facade)

---
+++ src/main/java/structural/facade/library/AudioMixer.java
@@ -0,0 +1,12 @@
+package structural.facade.library;
+
+import structural.facade.VideoFile;
+
+import java.io.File;
+
+public class AudioMixer {
+    public File fix(VideoFile result){
+        System.out.println("AudioMixer: fixing audio...");
+        return new File("tmp");
+    }
+}
---
+++ src/main/java/structural/facade/library/BitrateReader.java
@@ -0,0 +1,16 @@
+package structural.facade.library;
+
+import structural.facade.VideoFile;
+import structural.facade.compression.Codec;
+
+public class BitrateReader {
+    public static VideoFile read(VideoFile file, Codec codec) {
+        System.out.println("BitrateReader: reading file...");
+        return file;
+    }
+
+    public static VideoFile convert(VideoFile buffer, Codec codec) {
+        System.out.println("BitrateReader: writing file...");
+        return buffer;
+    }
+}
