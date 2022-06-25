81a24f7132fc8c023a0bbb5b36de0d823c8ef29f
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-25 12:38:01 +0100

    feat: add facade package (design pattern: Facade)

---
+++ src/main/java/structural/facade/facade/VideoConversionFacade.java
@@ -0,0 +1,30 @@
+package structural.facade.facade;
+
+import structural.facade.VideoFile;
+import structural.facade.compression.Codec;
+import structural.facade.compression.MPEG4CompressionCodec;
+import structural.facade.compression.OggCompressionCodec;
+import structural.facade.factory.CodecFactory;
+import structural.facade.library.AudioMixer;
+import structural.facade.library.BitrateReader;
+
+import java.io.File;
+
+public class VideoConversionFacade {
+    public File convertVideo(String fileName, String format) {
+        System.out.println("VideoConversionFacade: conversion started.");
+        VideoFile file = new VideoFile(fileName);
+        Codec sourceCodec = CodecFactory.extract(file);
+        Codec destinationCodec;
+        if (format.equals("mp4")) {
+            destinationCodec = new MPEG4CompressionCodec();
+        } else {
+            destinationCodec = new OggCompressionCodec();
+        }
+        VideoFile buffer = BitrateReader.read(file, sourceCodec);
+        VideoFile intermediateResult = BitrateReader.convert(buffer, destinationCodec);
+        File result = (new AudioMixer()).fix(intermediateResult);
+        System.out.println("VideoConversionFacade: conversion completed.");
+        return result;
+    }
+}
