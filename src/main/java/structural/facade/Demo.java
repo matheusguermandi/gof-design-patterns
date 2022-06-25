9221da72c759bbe8721704ed3b3092babdcf778c
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-25 12:38:20 +0100

    feat: add Demo (design pattern: Facade)

---
+++ src/main/java/structural/facade/Demo.java
@@ -0,0 +1,12 @@
+package structural.facade;
+
+import structural.facade.facade.VideoConversionFacade;
+
+import java.io.File;
+
+public class Demo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
