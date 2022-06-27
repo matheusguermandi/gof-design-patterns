f141e84d0dcdd44a1fd68ca16720b481cc4bb37b
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-27 19:05:09 +0100

    feat: add library package (design pattern: Proxy)

---
+++ src/main/java/structural/proxy/library/Video.java
@@ -0,0 +1,13 @@
+package structural.proxy.library;
+
+public class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video.";
+    }
+}
