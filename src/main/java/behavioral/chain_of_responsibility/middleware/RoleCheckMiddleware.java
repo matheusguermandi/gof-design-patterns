939b9ab2be19df2c62c78f6602919c906893fc1e
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-28 20:38:09 +0100

    feat: add middleware package (design pattern: Chain of Responsibility)

---
+++ src/main/java/behavioral/chain_of_responsibility/middleware/RoleCheckMiddleware.java
@@ -0,0 +1,12 @@
+package behavioral.chain_of_responsibility.middleware;
+
+public class RoleCheckMiddleware extends Middleware {
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
---
+++ src/main/java/behavioral/chain_of_responsibility/middleware/ThrottlingMiddleware.java
@@ -0,0 +1,27 @@
+package behavioral.chain_of_responsibility.middleware;
+
+public class ThrottlingMiddleware extends Middleware {
+    private int requestPerMinute;
+    private int request;
+    private long currentTime;
+
+    public ThrottlingMiddleware(int requestPerMinute) {
+        this.requestPerMinute = requestPerMinute;
+        this.currentTime = System.currentTimeMillis();
+    }
+
+    public boolean check(String email, String password) {
+        if (System.currentTimeMillis() > currentTime + 60_000) {
+            request = 0;
+            currentTime = System.currentTimeMillis();
+        }
+
+        request++;
+
+        if (request > requestPerMinute) {
+            System.out.println("Request limit exceeded!");
+            Thread.currentThread().stop();
+        }
+        return checkNext(email, password);
+    }
+}
