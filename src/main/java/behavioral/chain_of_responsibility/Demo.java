d67357ae45f2db26eb181c54eb8e508d44001c10
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-28 20:38:26 +0100

    feat: add Demo (design pattern: Chain of Responsibility)

---
+++ src/main/java/behavioral/chain_of_responsibility/Demo.java
@@ -0,0 +1,40 @@
+package behavioral.chain_of_responsibility;
+
+import behavioral.chain_of_responsibility.middleware.*;
+import behavioral.chain_of_responsibility.server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same
+        // components.
+        Middleware middleware = new ThrottlingMiddleware(2);
+        middleware.linkWith(new UserExistsMiddleware(server)).linkWith(new RoleCheckMiddleware());
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+}
