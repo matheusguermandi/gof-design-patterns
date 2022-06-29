3761f5a739df50bdf2522d7084aa8de4e7023783
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-29 15:51:17 +0100

    feat: add command package (design pattern: Command)

---
+++ src/main/java/behavioral/command/commands/Command.java
@@ -0,0 +1,23 @@
+package behavioral.command.commands;
+
+
+import behavioral.command.editor.Editor;
+
+public abstract class Command {
+    public Editor editor;
+    private String backup;
+
+    Command(Editor editor) {
+        this.editor = editor;
+    }
+
+    void backup() {
+        backup = editor.textField.getText();
+    }
+
+    public void undo() {
+        editor.textField.setText(backup);
+    }
+
+    public abstract boolean execute();
+}
---
+++ src/main/java/behavioral/command/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package behavioral.command.commands;
+
+import behavioral.command.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
