436e8546024366339486ecc11dee20483d6f5655
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-29 15:51:31 +0100

    feat: add Demo (design pattern: Command)

---
+++ src/main/java/behavioral/command/Demo.java
@@ -0,0 +1,10 @@
+package behavioral.command;
+
+import behavioral.command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
