d593b54c31ff6c610119b8cda844442cabad559f
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-30 18:14:12 +0100

    feat: add social_networks package (design pattern: Iterator)

---
+++ src/main/java/behavioral/iterator/social_networks/SocialNetwork.java
@@ -0,0 +1,9 @@
+package behavioral.iterator.social_networks;
+
+import behavioral.iterator.iterators.ProfileIterator;
+
+public interface SocialNetwork {
+    ProfileIterator createFriendsIterator(String profileEmail);
+
+    ProfileIterator createCoworkersIterator(String profileEmail);
+}
