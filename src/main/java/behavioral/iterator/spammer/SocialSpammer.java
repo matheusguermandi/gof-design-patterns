99b3f2c849d5c4651d091587e4dba0b57d41de44
matheusguermandi <matheus_guermandi@hotmail.com>
2022-06-30 18:14:23 +0100

    feat: add spammer package (design pattern: Iterator)

---
+++ src/main/java/behavioral/iterator/spammer/SocialSpammer.java
@@ -0,0 +1,37 @@
+package behavioral.iterator.spammer;
+
+
+import behavioral.iterator.iterators.ProfileIterator;
+import behavioral.iterator.profile.Profile;
+import behavioral.iterator.social_networks.SocialNetwork;
+
+public class SocialSpammer {
+    public SocialNetwork network;
+    public ProfileIterator iterator;
+
+    public SocialSpammer(SocialNetwork network) {
+        this.network = network;
+    }
+
+    public void sendSpamToFriends(String profileEmail, String message) {
+        System.out.println("\nIterating over friends...\n");
+        iterator = network.createFriendsIterator(profileEmail);
+        while (iterator.hasNext()) {
+            Profile profile = iterator.getNext();
+            sendMessage(profile.getEmail(), message);
+        }
+    }
+
+    public void sendSpamToCoworkers(String profileEmail, String message) {
+        System.out.println("\nIterating over coworkers...\n");
+        iterator = network.createCoworkersIterator(profileEmail);
+        while (iterator.hasNext()) {
+            Profile profile = iterator.getNext();
+            sendMessage(profile.getEmail(), message);
+        }
+    }
+
+    public void sendMessage(String email, String message) {
+        System.out.println("Sent message to: '" + email + "'. Message body: '" + message + "'");
+    }
+}
