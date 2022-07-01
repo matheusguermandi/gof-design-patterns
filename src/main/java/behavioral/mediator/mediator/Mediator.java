efab59e4b56be8e18cbed2bede8d5671a0d1ff4d
matheusguermandi <matheus_guermandi@hotmail.com>
2022-07-01 16:09:06 +0100

    feat: add mediator package (design pattern: Mediator)

---
+++ src/main/java/behavioral/mediator/mediator/Mediator.java
@@ -0,0 +1,19 @@
+package behavioral.mediator.mediator;
+
+import behavioral.mediator.components.Component;
+
+import javax.swing.*;
+
+public interface Mediator {
+    void addNewNote(Note note);
+    void deleteNote();
+    void getInfoFromList(Note note);
+    void saveChanges();
+    void markNote();
+    void clear();
+    void sendToFilter(ListModel listModel);
+    void setElementsList(ListModel list);
+    void registerComponent(Component component);
+    void hideElements(boolean flag);
+    void createGUI();
+}
