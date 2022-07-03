6135991c0c959ea604ef2f52b53f17984ad9c4c0
matheusguermandi <matheus_guermandi@hotmail.com>
2022-07-03 11:13:34 +0100

    feat: add Demo (design pattern: Observer)

---
+++ src/main/java/behavioral/observer/Demo.java
@@ -0,0 +1,20 @@
+package behavioral.observer;
+
+import behavioral.observer.editor.Editor;
+import behavioral.observer.listeners.EmailNotificationListener;
+import behavioral.observer.listeners.LogOpenListener;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
