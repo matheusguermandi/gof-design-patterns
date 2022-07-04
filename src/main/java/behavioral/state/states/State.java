66c7be01836b7032f4bfebd8fca982fa71d1b7d6
matheusguermandi <matheus_guermandi@hotmail.com>
2022-07-04 17:46:28 +0100

    feat: add states package (design pattern: State)

---
+++ src/main/java/behavioral/state/states/State.java
@@ -0,0 +1,24 @@
+package behavioral.state.states;
+
+
+import behavioral.state.ui.Player;
+
+/**
+ * Common interface for all states.
+ */
+public abstract class State {
+    Player player;
+
+    /**
+     * Context passes itself through the state constructor. This may help a
+     * state to fetch some useful context data if needed.
+     */
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
