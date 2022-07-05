d17a2aec24df3117113297097b350473bd01e15f
matheusguermandi <matheus_guermandi@hotmail.com>
2022-07-05 16:36:04 +0100

    feat: add strategies package (design pattern: Strategy)

---
+++ src/main/java/behavioral/strategy/strategies/PayStrategy.java
@@ -0,0 +1,9 @@
+package behavioral.strategy.strategies;
+
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
+}
